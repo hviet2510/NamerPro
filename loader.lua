ca3802cefcce4e68930c085e086018ad72c4456b
hviet2510 <vieth1393@gmail.com>
2025-06-23 15:11:58 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hviet2510/NamerPro/main/main.lua"))()
+local success, response = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/hviet2510/NamerPro/main/main.lua")
+end)
+
+if success then
+    loadstring(response)()
+else
+    warn("Không thể tải main.lua từ GitHub!")
+end
