e447408907fd5b94d4e4eb2055c1cffe3ae2699e
hviet2510 <vieth1393@gmail.com>
2025-06-28 02:29:16 +0700

    Update enemylist.lua

--- modules/enemylist.lua
+++ modules/enemylist.lua
@@ -2,5 +2,9 @@ return {
     {Level = 1, Mob = "Bandit", Quest = "BanditQuest1", CFrame = CFrame.new(1060, 16, 1547)},
     {Level = 10, Mob = "Monkey", Quest = "JungleQuest", CFrame = CFrame.new(-1613, 36, 144)},
     {Level = 15, Mob = "Gorilla", Quest = "JungleQuest", CFrame = CFrame.new(-1238, 6, -507)},
-    -- Thêm đầy đủ Sea 1 ở đây
+    {Level = 20, Mob = "Pirate", Quest = "BuggyQuest1", CFrame = CFrame.new(-1140, 4, 3820)},
+    {Level = 30, Mob = "Brute", Quest = "BuggyQuest1", CFrame = CFrame.new(-1250, 4, 4300)},
+    {Level = 40, Mob = "Desert Bandit", Quest = "DesertQuest", CFrame = CFrame.new(932, 7, 4483)},
+    -- Thêm các quái tiếp theo tại đây...
+    {Level = 700, Mob = "Galley Captain", Quest = "FountainQuest", CFrame = CFrame.new(5733, 42, 4772)},
 }
