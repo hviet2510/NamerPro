9ef4330e0d776f6ae7cf53e095df59d8c8b9661b
hviet2510 <vieth1393@gmail.com>
2025-06-28 01:31:07 +0700

    Update Buttons.lua

--- module/Buttons.lua
+++ module/Buttons.lua
@@ -1,17 +1,15 @@
-return function(DrRayLibrary)
-    local Buttons = {}
+local Buttons = {}
 
-    function Buttons.Create(tab, text, callback)
-        tab.newButton(text, "Button created", callback)
-    end
-
-    function Buttons.CreateToggle(tab, text, default, callback)
-        tab.newToggle(text, "Toggle created", default, callback)
-    end
-
-    function Buttons.CreateDropdown(tab, text, options, callback)
-        tab.newDropdown(text, "Dropdown created", options, callback)
-    end
-
-    return Buttons
+function Buttons.Create(tab, text, callback)
+    tab.newButton(text, "Button created", callback)
 end
+
+function Buttons.CreateToggle(tab, text, default, callback)
+    tab.newToggle(text, "Toggle created", default, callback)
+end
+
+function Buttons.CreateDropdown(tab, text, options, callback)
+    tab.newDropdown(text, "Dropdown created", options, callback)
+end
+
+return Buttons
