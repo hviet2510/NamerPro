6256709f4e1358c4af2bf4d5411038f5aa96e94d
hviet2510 <vieth1393@gmail.com>
2025-06-28 03:34:56 +0700

    Update buttons.lua

--- modules/buttons.lua
+++ modules/buttons.lua
@@ -1,15 +1,7 @@
 local Buttons = {}
 
-function Buttons.Create(tab, text, callback)
-    tab.newButton(text, "Click để thực thi", callback)
-end
-
-function Buttons.CreateToggle(tab, text, default, callback)
-    tab.newToggle(text, "Bật/tắt", default, callback)
-end
-
-function Buttons.CreateDropdown(tab, text, options, callback)
-    tab.newDropdown(text, "Chọn 1 tuỳ chọn", options, callback)
+function Buttons.Create(tab, name, callback)
+    tab.newButton(name, "Button desc", callback)
 end
 
 return Buttons
