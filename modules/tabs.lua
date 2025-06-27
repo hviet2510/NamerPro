47cffc30c797ecce223b6d7d322710cdccd1fe85
hviet2510 <vieth1393@gmail.com>
2025-06-28 02:39:34 +0700

    Update tabs.lua

--- modules/tabs.lua
+++ modules/tabs.lua
@@ -1,12 +1,8 @@
 local Tabs = {}
 
 function Tabs.Create(window, name, imageId)
-    if window and window.newTab then
-        return window.newTab(name, imageId)
-    else
-        warn("[Tabs] Window không hợp lệ hoặc thiếu newTab")
-        return nil
-    end
+    -- window ở đây chính là DrRayLibrary, vì DrRay gọi newTab trực tiếp từ lib
+    return window.newTab(name, imageId)
 end
 
 return Tabs
