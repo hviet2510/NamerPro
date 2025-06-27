c24608827d08f7766be58e2bf5060a25c9420436
hviet2510 <vieth1393@gmail.com>
2025-06-28 02:34:46 +0700

    Update buttons.lua

--- modules/buttons.lua
+++ modules/buttons.lua
@@ -1,15 +1,43 @@
 local Buttons = {}
 
-function Buttons.Create(tab, name, callback)
-    tab.newButton(name, "Button action", callback)
+function Buttons.Create(tab, text, callback)
+    if tab and tab.newButton then
+        tab.newButton(text, "", callback)
+    else
+        warn("[Buttons] Tab không hợp lệ hoặc thiếu newButton")
+    end
 end
 
-function Buttons.CreateToggle(tab, name, default, callback)
-    tab.newToggle(name, "Toggle action", default, callback)
+function Buttons.CreateToggle(tab, text, default, callback)
+    if tab and tab.newToggle then
+        tab.newToggle(text, "", default, callback)
+    else
+        warn("[Buttons] Tab không hợp lệ hoặc thiếu newToggle")
+    end
 end
 
-function Buttons.CreateDropdown(tab, name, options, callback)
-    tab.newDropdown(name, "Dropdown action", options, callback)
+function Buttons.CreateDropdown(tab, text, list, callback)
+    if tab and tab.newDropdown then
+        tab.newDropdown(text, "", list, callback)
+    else
+        warn("[Buttons] Tab không hợp lệ hoặc thiếu newDropdown")
+    end
+end
+
+function Buttons.CreateInput(tab, text, callback)
+    if tab and tab.newInput then
+        tab.newInput(text, "", callback)
+    else
+        warn("[Buttons] Tab không hợp lệ hoặc thiếu newInput")
+    end
+end
+
+function Buttons.CreateLabel(tab, text)
+    if tab and tab.newLabel then
+        tab.newLabel(text)
+    else
+        warn("[Buttons] Tab không hợp lệ hoặc thiếu newLabel")
+    end
 end
 
 return Buttons
