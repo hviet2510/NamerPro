0a22d54de2fbe443ff52dadeedefa44702e31623
hviet2510 <vieth1393@gmail.com>
2025-06-28 02:48:42 +0700

    Update tabs.lua

--- modules/tabs.lua
+++ modules/tabs.lua
@@ -1,7 +1,6 @@
 local Tabs = {}
 
 function Tabs.Create(window, name, imageId)
-    -- window ở đây chính là DrRayLibrary, vì DrRay gọi newTab trực tiếp từ lib
     return window.newTab(name, imageId)
 end
 
