099d7476f9d8489870a15d06190c2d2503c717f6
hviet2510 <vieth1393@gmail.com>
2025-06-28 02:06:04 +0700

    Create enemylist.lua

---
+++ modules/enemylist.lua
@@ -0,0 +1,6 @@
+return {
+    {Level = 1, Mob = "Bandit", Quest = "BanditQuest1", CFrame = CFrame.new(1060, 16, 1547)},
+    {Level = 10, Mob = "Monkey", Quest = "JungleQuest", CFrame = CFrame.new(-1613, 36, 144)},
+    {Level = 15, Mob = "Gorilla", Quest = "JungleQuest", CFrame = CFrame.new(-1238, 6, -507)},
+    -- Thêm đầy đủ Sea 1 ở đây
+}
